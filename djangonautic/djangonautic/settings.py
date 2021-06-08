a70659867ab2b57bbfcce4d5a52f2904a2d91db9
vishnu-vasan <58689453+vishnu-vasan@users.noreply.github.com>
2021-06-09 00:17:30 +0530

    Update settings.py

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*mc#(uxu-yn8&55tf&136hh3!z(eewxi=k_ysuisvezdop^&68'
+#SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
